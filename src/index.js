a0f79308183245789adecd8a0dd7de2cee14f889
IhorKozloff <ik.eyeshield21@gmail.com>
2022-03-19 00:12:26 +0200

    уже рендерит на страницу по поиску

--- src/index.js
+++ src/index.js
@@ -13,27 +13,36 @@ function onSubmitSearchForm (e) {
     const searchedMovie = e.currentTarget.elements.movie.value;
     console.log(searchedMovie)
     
-    searchAPIName(searchedMovie);
+    searchAPIName(searchedMovie).then(result => {
+      console.log(result[0])
+      
+       let forRender = "";
+        result.map(item => {
+          forRender += galleryMurkUpMaker(item);
+      })
+      refs.galeryEl.innerHTML = forRender;
+    })
+    
+    
 
 }
 
 
 function searchAPIName (name) {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=8b9c2b35d1bc0d9e8879c4faa9dd8b75&query=${name}`).then(response => response.json()).then(data => {
-    console.log(data)
-})
+   return fetch(`https://api.themoviedb.org/3/search/movie?api_key=8b9c2b35d1bc0d9e8879c4faa9dd8b75&query=${name}`).then(response => response.json()).then(data => data.results) 
 };
 
-function galleryRender ({poster_path, title, release_date, genre_ids, vote_average}) {
-refs.galeryEl.innerHTML = `
+function galleryMurkUpMaker ({poster_path, title, genre_ids, release_date, vote_average}) {
+ 
+return  `
         <div class="card">
-            <img src=${poster_path}>
+            <img src=https://image.tmdb.org/t/p/original${poster_path} width="280" height="398">
             <div class="card__description">
               <h4 class="movie-name">${title}</h4>
               <div class="movie-spesification">
-                <span class="movie-genre">Drama, Action</span>
-                <span class="movie-release">2020</span>
-                <span class="movie-rating">10.0</span>
+                <span class="movie-genre">${genre_ids}</span>
+                <span class="movie-release">${release_date}</span>
+                <span class="movie-rating">${vote_average}</span>
               </div>
             </div>
         </div>
